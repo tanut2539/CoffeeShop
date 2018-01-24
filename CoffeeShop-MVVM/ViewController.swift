2bda579fea6ecf525593fb23645b13ed26b1cd11
tanut2539 <tanut.leel@bumail.net>
2018-01-24 23:56:42 +0700

    Initial Commit

---
+++ CoffeeShop-MVVM/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  CoffeeShop-MVVM
+//
+//  Created by tanut2539 on 1/24/18.
+//  Copyright © 2018 Tanut Leelaparsert. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
---
+++ CoffeeShop-MVVMTests/CoffeeShop_MVVMTests.swift
@@ -0,0 +1,36 @@
+//
+//  CoffeeShop_MVVMTests.swift
+//  CoffeeShop-MVVMTests
+//
+//  Created by tanut2539 on 1/24/18.
+//  Copyright © 2018 Tanut Leelaparsert. All rights reserved.
+//
+
+import XCTest
+@testable import CoffeeShop_MVVM
+
+class CoffeeShop_MVVMTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
